040c217a71577700937dfcaeb2f601ffb89fc00a
nalo <ThePadna@users.noreply.github.com>
2015-05-03 15:49:00 +0100

    cmd,obj,resources

---
+++ src/main/java/obj/Stall.java
@@ -0,0 +1,14 @@
+package obj;
+
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Stall implements ConfigurationSerializable {
+
+    public Map<String, Object> serialize() {
+        HashMap map = new HashMap<String, Object>();
+        return map;
+    }
+}
